e43b21f2254f3527af03e209004a35b1b1a65c82
kaddour abdelaziz <47844266+Sukikiroi@users.noreply.github.com>
2020-07-30 15:22:46 +0000

    Add files via upload

---
+++ cssFunvtion.js
@@ -0,0 +1,34 @@
+  //Css of All Sliders
+  export  const Sliders = {
+  
+    width:'680px'
+   
+  };
+  //Css of Button Oui
+  export  const OUIButton = {
+  
+    width:'150px'
+   
+  };
+  //Css of Button Non
+  export  const NONButton = {
+  
+    width:'150px'
+   
+  };
+  //Css of Textfiled
+  export  const Textfiledcss = {
+  
+    border: '1px ridge  Black',
+    borderRadius:8,
+    height:'45px',
+    right:"320px",
+    width:'117px'
+  };
+
+  //Css of The Card
+  export const card = {
+    height: '1243px',
+    width:'831px',
+    fontFamily: "Arial"
+  };
